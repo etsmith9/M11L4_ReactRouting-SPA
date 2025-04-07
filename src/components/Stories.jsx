cfd3ac4b6e0dcfebe5ca48387e87b67a9984af05
Emily Smith <etsmith9@gmail.com>
2025-04-07 15:38:46 -0400

    Update Stories.jsx

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,10 +1,9 @@
-const Stories = () => {
-    return (
-        <div>
-            <h2>Stories</h2>
-            <p>This component is under construction. Check back later!</p>
-        </div>
-    );
-};
-
-export default Stories;
+const Stories = () => (
+    <div>
+      <h2>Stories</h2>
+      <p>This component is under construction. Check back later!</p>
+    </div>
+  );
+  
+  export default Stories;
+  
