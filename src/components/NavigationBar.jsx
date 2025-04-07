3eac6175789b7d64f716102076f77f1fd6c9c270
Emily Smith <etsmith9@gmail.com>
2025-04-07 15:37:36 -0400

    Update NavigationBar.jsx

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,15 +1,23 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink } from 'react-router-dom';
 
+// Implementing Navigation Links
 
-function NavigationBar() {
-   return (
-       <nav className="clearfix"> 
-       <Link to='/'>Home</Link>
-       <Link to='/BrowseCharacters'>Browse Characters</Link>
-       <Link to='/CharacterDetail'>Character Details</Link>
-       <Link to='/Stories'>Stories</Link>
-       
-       </nav>
-   )
-}
+const NavigationBar = () => (
+  <nav>
+    <ul>
+      <li>
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/browse">Browse Pokémon</NavLink>
+      </li>
+      <li>
+        <NavLink to="/stories">Comics</NavLink>
+      </li>
+    </ul>
+  </nav>
+);
+
 export default NavigationBar;
