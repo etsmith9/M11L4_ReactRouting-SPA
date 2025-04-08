995d329d4647f295922edc2d19096502ae1f7c25
Emily Smith <etsmith9@gmail.com>
2025-04-07 20:12:23 -0400

    Update NotFound.jsx

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -7,5 +7,4 @@ const NotFound = () => (
   
   export default NotFound;
 
-  // This component is a simple 404 Not Found page that will be displayed when the user navigates to a route that doesn't exist in the application. It provides a user-friendly message indicating that the requested page could not be found. The component can be styled further to match the overall design of the application.
   
